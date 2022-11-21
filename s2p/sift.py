fdab025f2990b79113a253b4c615045671ed8aa9
Tim <35193581+tonzowonzo@users.noreply.github.com>
2022-11-21 21:59:20 +0100

    Bugfix

--- s2p/sift.py
+++ s2p/sift.py
@@ -291,7 +291,7 @@ def matches_on_rpc_roi(im1, im2, rpc1, rpc2, x, y, w, h,
                                                  max_err=0.3)[0]
         matches = matches[inliers]
         
-        if len(matches) == 0 and matches.ndim != 2:
+        if len(matches) == 0 or matches.ndim != 2:
             print("WARNING: sift.matches_on_rpc_roi: found no matches.")
             return None
         
@@ -303,7 +303,7 @@ def matches_on_rpc_roi(im1, im2, rpc1, rpc2, x, y, w, h,
         p2[:, 1] += y2
         matches = np.hstack((p1, p2))
         
-        if len(matches) == 0 and matches.ndim != 2:
+        if len(matches) == 0 or matches.ndim != 2:
             print("WARNING: sift.matches_on_rpc_roi: found no matches.")
             return None
 
