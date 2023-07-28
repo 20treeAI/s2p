a2f8e59c0613f3cb69c317264dd89c7c63670d9a
David de Meij <daviddemeij@gmail.com>
2023-07-28 07:59:36 +0300

    Also replace 0s with -9999

--- s2p/__init__.py
+++ s2p/__init__.py
@@ -537,6 +537,7 @@ def plys_to_dsm(tile, cfg):
                                                     radius=cfg['dsm_radius'],
                                                     sigma=cfg['dsm_sigma'])
     raster = np.nan_to_num(raster, -9999.)
+    raster[raster == 0] = -9999.
     profile.update(nodata=-9999.)
 
     # save output image with utm georeferencing
