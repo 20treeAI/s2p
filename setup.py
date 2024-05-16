f02f1453a052d4136f4a50ba0c1d802596e49517
Roelof van Dijk <roelof.vandijk@overstory.com>
2024-05-16 08:54:50 +0200

    test: point to new rpcm branch

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ requirements = [
     "plyfile",
     "plyflatten>=0.2.0",
     "ransac",
-    "rpcm @ git+https://github.com/20treeAI/rpcm.git@chore/rebased_on_upstream",
+    "rpcm @ git+https://github.com/20treeAI/rpcm.git@chore/rebased",
     "srtm4",
     "requests",
     "opencv-python",
