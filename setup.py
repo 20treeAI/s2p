8ff0e877ea0ae1f14e3177d45dbdaf0bd76fa478
Roelof van Dijk <roelof.vandijk@overstory.com>
2024-05-23 11:28:04 +0200

    fix: add comment

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ try:
 except ImportError:
     BdistWheel = None
 
-
+# rpcm not yet 1.4.11 because s2p-pipelines needs a 1 pixel offset fix in test data
 requirements = ['numpy>=1.22.2',
                 'scipy',
                 'rasterio[s3]==1.3.8',
