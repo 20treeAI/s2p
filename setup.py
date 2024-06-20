7800450d8187913a0e27162d38153c3f45fc28da
Roelof van Dijk <roelof.vandijk@overstory.com>
2024-06-20 13:55:00 +0200

    chore: allow numpy 2

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ try:
 except ImportError:
     BdistWheel = None
 
-requirements = ['numpy<2',
+requirements = ['numpy',
                 'scipy',
                 'rasterio[s3]>=1.3.8',
                 'utm',
@@ -63,7 +63,7 @@ extras_require = {
 }
 
 setup(name="s2p",
-      version="1.6.9",
+      version="1.6.10",
       description="Satellite Stereo Pipeline.",
       long_description=readme(),
       long_description_content_type='text/markdown',
